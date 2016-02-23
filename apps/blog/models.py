d2dbe344f14fe1048edeae257ac5c142e3e9f3e5
kregoslup <mateusz.olczynski@gmail.com>
2016-02-23 17:51:24 +0100

    apps models: changed charfield nullable=False and added max_length

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,21 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
-import datetime
+from urllib.parse import urljoin
 from apps.posts.models import Post
 
 
 class GithubUser(models.Model):
-    user = models.OneToOneField(User, related_name='user')
-    github_username = models.CharField(blank=False)
-    access_token = models.TextField(blank=True)
-    refresh_token = models.CharField(blank=True)
+    github_username = models.CharField(blank=False, null=False, max_length=200)
+    access_token = models.TextField(blank=True, null=False, max_length=300)
+    refresh_token = models.TextField(blank=False, null=False, max_length=300)
 
 
 class Blog(models.Model):
-    title = models.CharField(max_length=100, unique=True)
+    title = models.CharField(max_length=100, unique=True, null=False, blank=False)
     owner = models.ForeignKey(GithubUser, on_delete=models.CASCADE)
-    github_url = models.URLField()
+    _repository_name = models.CharField(max_length=300, null=False, blank=False)
 
+    @property
+    def repository_name(self):
+        return self._repository_name
+
+    @repository_name.setter
+    def repository_name(self, value):
+        self._repository_name = urljoin(self.owner.github_username, value)
 
     def __unicode__(self):
         return '%s' % self.title
--- apps/posts/models.py
+++ apps/posts/models.py
@@ -3,14 +3,21 @@ from apps.blog.models import Blog
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    creation_date = models.DateField()
-    body = models.TextField()
+    title = models.CharField(max_length=200, unique=True, null=False, blank=False)
+    creation = models.DateField()
+    update = models.DateField()
+    body = models.TextField(max_length=3000, null=False, blank=False)
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
-
 
     def __unicode__(self):
         return '%s' % self.title
 
     class Meta:
         ordering = ('creation_date',)
+
+
+class Commit(models.Model):
+    hash = models.CharField(blank=False, null=False, max_length=10)
+    title = models.CharField(blank=False, null=False, max_length=50)
+    data = models.TextField(blank=False, null=False, max_length=3000)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
