a203faf1ece53a121b8f75d2caf0c4d4e465d2a2
kregoslup <mateusz.olczynski@gmail.com>
2016-02-24 12:54:38 +0100

    Fix unused imports

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,11 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
 from urllib.parse import urljoin
-from apps.posts.models import Post
 
 
 class GithubUser(models.Model):
-    github_username = models.CharField(blank=False, null=False, max_length=200)
+    username = models.CharField(blank=False, null=False, max_length=200)
     access_token = models.TextField(blank=True, null=False, max_length=300)
     refresh_token = models.TextField(blank=False, null=False, max_length=300)
 
@@ -13,15 +11,11 @@ class GithubUser(models.Model):
 class Blog(models.Model):
     title = models.CharField(max_length=100, unique=True, null=False, blank=False)
     owner = models.ForeignKey(GithubUser, on_delete=models.CASCADE)
-    _repository_name = models.CharField(max_length=300, null=False, blank=False)
+    name = models.CharField(max_length=300, null=False, blank=False)
 
     @property
-    def repository_name(self):
-        return self._repository_name
+    def name(self):
+        return urljoin('http://github.com', self.owner.username + '/' + self.name)
 
-    @repository_name.setter
-    def repository_name(self, value):
-        self._repository_name = urljoin(self.owner.github_username, value)
-
-    def __unicode__(self):
-        return '%s' % self.title
+    def __repr__(self):
+        return '<Blog: %r>' % self.title
