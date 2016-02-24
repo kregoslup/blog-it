7e4f63742704bdda42cdc86b375020c25f206992
kregoslup <mateusz.olczynski@gmail.com>
2016-02-24 18:29:20 +0100

    Add repr method to models & remove unicode

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -4,18 +4,24 @@ from urllib.parse import urljoin
 
 class GithubUser(models.Model):
     username = models.CharField(blank=False, null=False, max_length=200)
-    access_token = models.TextField(blank=True, null=False, max_length=300)
-    refresh_token = models.TextField(blank=False, null=False, max_length=300)
+    access_token = models.CharField(blank=True, null=False, max_length=300)
+    refresh_token = models.CharField(blank=False, null=False, max_length=300)
+
+    def __repr__(self):
+        return '<User(username=%r, a_token=%r)' % self.username, self.access_token
 
 
 class Blog(models.Model):
-    title = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    title = models.CharField(max_length=100, unique=True, null=False,
+                             blank=False)
     owner = models.ForeignKey(GithubUser, on_delete=models.CASCADE)
-    name = models.CharField(max_length=300, null=False, blank=False)
+    full_name = models.CharField(max_length=300, null=False, blank=False)
 
     @property
-    def name(self):
-        return urljoin('http://github.com', self.owner.username + '/' + self.name)
+    def full_name(self):
+        return urljoin('http://github.com',
+                       self.owner.username + '/' + self.full_name)
 
     def __repr__(self):
-        return '<Blog: %r>' % self.title
+        return '<Blog(title=%r, owner=%r full_name=%r)>' % self.title,\
+               self.owner.username, self.full_name
--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,23 +1,30 @@
 from django.db import models
-from apps.blog.models import Blog
+from apps.blog.models import Blog, GithubUser
 
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True, null=False, blank=False)
-    creation = models.DateField()
-    update = models.DateField()
+    created = models.DateField()
+    updated = models.DateField()
     body = models.TextField(max_length=3000, null=False, blank=False)
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    author = models.ForeignKey(GithubUser, on_delete=models.CASCADE)
 
-    def __unicode__(self):
-        return '%s' % self.title
+    def __repr__(self):
+        return 'Post: %r Author: %r>' % self.title, self.author.username
 
     class Meta:
         ordering = ('creation_date',)
 
 
 class Commit(models.Model):
-    hash = models.CharField(blank=False, null=False, max_length=10)
+    hash = models.CharField(blank=False, null=False, max_length=40)
     title = models.CharField(blank=False, null=False, max_length=50)
     data = models.TextField(blank=False, null=False, max_length=3000)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(GithubUser, on_delete=models.CASCADE)
+
+    def __repr__(self):
+        return '<Commit(hash=%r, title=%r, post=%r, author=%r)' % self.hash,\
+               self.title, self.post, self.author.username
+
