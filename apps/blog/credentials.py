620c0b6d2291855bfefde73e2cbe1026297117e8
kregoslup <mateusz.olczynski@gmail.com>
2016-02-25 02:33:35 +0100

    Add mock credentials for tests

---
+++ apps/blog/credentials.py
@@ -0,0 +1,6 @@
+AUTHORIZATION_URL = 'https://github.com/login/oauth/authorize'
+TOKEN_URL = 'https://github.com/login/oauth/access_token'
+CLIENT_ID = ''
+REDIRECT_URL = ''
+CLIENT_SECRET = ''
+SCOPE = ''
