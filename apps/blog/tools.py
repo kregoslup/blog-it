da693141878b1faf110a9702298a74ebbcc705a5
kregoslup <mateusz.olczynski@gmail.com>
2016-02-26 01:14:27 +0100

    Change to function based tools

--- apps/blog/tools.py
+++ apps/blog/tools.py
@@ -1,35 +1,37 @@
 from requests_oauthlib import OAuth2Session
 from apps.blog.credentials import *
+from django.shortcuts import redirect
+
+token_dict = {}
 
 
-class OAuth2Signup:
-    signer = {}
-    client_id = CLIENT_ID
-    scope = SCOPE
-    authorization_base_url = AUTHORIZATION_URL
-    redirect_uri = REDIRECT_URL
-    token_url = TOKEN_URL
-    client_secret = CLIENT_SECRET
+def sign_up():
+    signer = OAuth2Session(CLIENT_ID, scope=SCOPE)
+    authorization_url, state = signer.authorization_url(
+        AUTHORIZATION_URL)
+    token_dict.update({state: signer})
+    return authorization_url
 
-    @staticmethod
-    def sign_up():
-        signer = OAuth2Session(OAuth2Signup.client_id, scope=OAuth2Signup.scope)
-        authorization_url, state = signer.authorization_url(
-            OAuth2Signup.authorization_base_url)
-        OAuth2Signup.signer.update({state: signer})
-        return authorization_url
 
-    @staticmethod
-    def fetch_token(request):
-        signer = OAuth2Signup.signer.get(request.META.state)
-        token = signer.fetch_token(OAuth2Signup.token_url,
-                                        client_secret=OAuth2Signup.client_secret,
-                                        authorization_response=OAuth2Signup.redirect_uri)
+def fetch_token(request):
+    signer = token_dict.get(request.META.state)
+    token = signer.fetch_token(TOKEN_URL,
+                               client_secret=CLIENT_SECRET,
+                               authorization_response=REDIRECT_URL)
 
-        return token, signer
+    return token, signer
 
-    @staticmethod
-    def get_username(signer):
-        username = signer.get('https://api.github.com/user').json().get(
-            "login")
-        return username
+
+def get_username(signer):
+    username = signer.get('https://api.github.com/user').json().get(
+        "login")
+    return username
+
+
+def authorization(request):
+    if request.json().get("code") is None:
+        return redirect(sign_up())
+    else:
+        token, signer = fetch_token(request)
+        username = get_username(signer)
+        return token, username
