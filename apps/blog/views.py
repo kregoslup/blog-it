920161cc410da3829413c0cfb32aabe07cbc56e8
kregoslup <mateusz.olczynski@gmail.com>
2016-02-26 01:21:20 +0100

    Simplify views & removed redundant class

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,27 +1,18 @@
-from apps.blog.tools import *
-from rest_framework.decorators import throttle_classes
+from rest_framework.decorators import throttle_classes, api_view
 from rest_framework.throttling import UserRateThrottle
-from rest_framework.views import APIView
 from apps.blog.serializers import UserSerializer
 from apps.blog.models import User
-from apps.blog.tools import OAuth2Signup
+from apps.blog.tools import authorization
 
 
 class TenPerDayUserThrottle(UserRateThrottle):
     rate = '10/day'
 
 
-class ApiEndpoint(APIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    sign_client = OAuth2Signup
-
-    @throttle_classes([TenPerDayUserThrottle])
-    def get(self, request):
-        if request.META.code is None:
-            authorization_url = self.sign_client.sign_up()
-            # TODO: Redirect to authorization_url
-        else:
-            token, username = self.sign_client.fetch_token(request)
-            # TODO: Drop sign_client.signer dict entry where state is the same
-            # as in request.get.META.state after serializing and saving to database
+@throttle_classes([TenPerDayUserThrottle])
+@api_view(['GET'])
+def token_query(request):
+    token, username = authorization(request)
+    serializer = UserSerializer
+    if serializer.is_valid():
+        serializer.save(username=username, access_token=token)
