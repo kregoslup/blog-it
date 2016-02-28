101667ace52db4d759a2167d84426777d209892a
kregoslup <mateusz.olczynski@gmail.com>
2016-02-29 00:59:41 +0100

    Add serialization

--- apps/blog/serializers.py
+++ apps/blog/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from apps.blog.models import User
+from apps.blog.models import User, Blog
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = '__all__'
+
+
+class BlogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
         fields = '__all__'
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,11 +1,27 @@
-from apps.blog.serializers import UserSerializer
+from apps.blog.serializers import UserSerializer, BlogSerializer
 from apps.blog.models import User
-from django.shortcuts import redirect
 from github import oauth2, profile
 
 
+def chose_repo(request):
+    if request.method == 'POST':
+        repo = request.POST['repository']
+        title = request.POST['title']
+        owner = request.session['username']
+        serializer = BlogSerializer(title=title, owner=owner, repo=repo)
+        if serializer.is_valid():
+            serializer.save()
+# create hook
+
+
 def profile_info(request):
-    profile.get_username(request)
+    user, gh = profile.get_username(request)
+    serializer = UserSerializer(username=user,
+                                access_token=request.session['oauth2_token'])
+    if serializer.is_valid():
+        serializer.save()
+    repos = profile.get_all_repos(gh)
+    return {user: repos}
 
 
 def oauth_callback(request):
