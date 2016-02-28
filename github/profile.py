9d691e163ee6e101b63331b5c1d6ee565887744e
kregoslup <mateusz.olczynski@gmail.com>
2016-02-29 00:58:33 +0100

    Add get_all_repos & get_username

--- github/profile.py
+++ github/profile.py
@@ -1,7 +1,23 @@
 import github3
+from rest_framework import status
+from rest_framework.response import Response
+
+
+def get_all_repos(gh):
+    repositories = [name for name in gh.all_repositories['name']]
+    if not repositories:
+        return Response({"message": "No repositories"},
+                        status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return repositories
 
 
 def get_username(request):
     gh = github3.login(token=request.session['token'])
     user = gh.me()['login']
-    return user
+    if not user:
+        return Response({"message": "Invalid user"},
+                        status=status.HTTP_400_BAD_REQUEST)
+    else:
+        request.session['username'] = user
+        return user, gh
