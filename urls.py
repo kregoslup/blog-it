168610f3f563f4355bae849eefe0bd316459d99b
kregoslup <mateusz.olczynski@gmail.com>
2016-02-28 23:59:03 +0100

    Fix typo

--- urls.py
+++ urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url, include
-from apps.blog.views import profile
+from apps.blog.views import profile_info
 
 urlpatterns = [
     url(r'^$',),
-    url(r'^profile/$', profile),
+    url(r'^profile/$', profile_info),
     url(r'^api/github/$', include('apps.blog.urls')),
 ]
