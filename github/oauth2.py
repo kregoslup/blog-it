bd7918de7cc1df31e959a6ba193c6292ed1857f1
kregoslup <mateusz.olczynski@gmail.com>
2016-03-01 18:49:51 +0100

    Fix login - tested and working

--- github/oauth2.py
+++ github/oauth2.py
@@ -12,18 +12,16 @@ def get_authorization_url():
     github = OAuth2Session(CLIENT_ID, scope=SCOPE)
     authorization_url, state = github.authorization_url(
         AUTHORIZATION_URL)
-    s = SessionStore()
-    s['state'] = state
     if not all((authorization_url, state)):
         return Response({"message": "Invalid authorization url"},
                         status=status.HTTP_400_BAD_REQUEST)
     else:
-        redirect(authorization_url)
+        return authorization_url
 
 
 def get_token(request):
     code = request.GET['code']
-    state = request.session['state']
+    state = request.GET['state']
     github = OAuth2Session(CLIENT_ID, scope=SCOPE, state=state)
     token = github.fetch_token(TOKEN_URL,
                                client_secret=CLIENT_SECRET,
@@ -34,6 +32,4 @@ def get_token(request):
         return Response(data={"message": "Invalid token"},
                         status=status.HTTP_400_BAD_REQUEST)
     else:
-        request.session['oauth_token'] = token
-        redirect('profile')
-
+        return token
