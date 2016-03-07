7c7f2fedb436d00efb757b0c02db97cae7e240c0
kregoslup <mateusz.olczynski@gmail.com>
2016-03-07 18:06:28 +0100

    Change ForeignKey in Commit model

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from apps.blog.models import Blog, User
 from urllib.parse import urljoin
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from apps.posts import tasks
 
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True, null=False,
                              blank=False)
-    created = models.DateField()
-    updated = models.DateField()
+    created = models.DateField(auto_created=True)
+    updated = models.DateField(auto_now_add=True)
     body = models.TextField(max_length=3000, null=False, blank=False)
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -29,7 +32,7 @@ class Commit(models.Model):
     hash = models.CharField(blank=False, null=False, max_length=100)
     title = models.CharField(blank=False, null=False, max_length=50)
     data = models.TextField(blank=False, null=False, max_length=3000)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
 
     def __repr__(self):
         return '<Commit(hash=%r, title=%r, post=%r, author=%r)' % (self.hash,
