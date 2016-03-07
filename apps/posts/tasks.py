cf3dd0340fb318fb64e32dd4694c5385d0bebd2b
kregoslup <mateusz.olczynski@gmail.com>
2016-03-08 00:50:31 +0100

    Add syncing posts & commits - bulk_create && parsing webhook data

--- apps/posts/tasks.py
+++ apps/posts/tasks.py
@@ -1,13 +1,44 @@
 from apps.blog.models import Blog, User
-from apps.posts.models import Commit
+from apps.posts.models import Commit, Post
 from celery.task import task
-from apps.posts.serializers import CommitSerializer
 import github3
+from github.repos import download_file
+from django.core.exceptions import ObjectDoesNotExist
+
+
+# TODO: Sanity checks
 
 
 @task
 def sync_posts(data):
-    pass
+    u = User.objects.get(username=data['username'])
+    b = Blog.objects.get(name=data['repo'], owner=u)
+    posts_bulk_create_data = []
+    for file in data['files']:
+        post_body = download_file(file['raw_url'], u.access_token)
+        posts_bulk_create_data.append(Post(title=file['filename'].split('.')[-1],
+                                           body=post_body, blog=b, author=u))
+    Post.objects.bulk_create(posts_bulk_create_data,
+                             batch_size=len(posts_bulk_create_data))
+
+
+@task
+def sync_commits(data):
+    u = User.objects.get(username=data['username'])
+    b = Blog.objects.get(name=data['repo'], owner=u)
+    commit_bulk_create_data = []
+    for commit in data:
+        commit_bulk_create_data.append(Commit(hash=commit['hash'],
+                                              title=commit['title'], blog=b))
+    if commit_bulk_create_data:
+        Post.objects.bulk_create(commit_bulk_create_data,
+                                 batch_size=len(commit_bulk_create_data))
+
+    gh = github3.GitHub(token=u.access_token)
+    gh_repo = gh.repository(u.username, b.name)
+    commit_additional_data = gh_repo.commit(data['hash'])
+    data.update({"files": commit_additional_data.as_dict()['files']})
+    sync_posts.delay(data)
 
 
 @task
@@ -15,17 +46,8 @@ def parse_webhook(request_data):
     commits = [commit for commit in request_data['commits']]
     repo = request_data["repository"]['name']
     username = request_data['owner']['name']
-
-    u = User.objects.get(username=username)
-    b = Blog.objects.get(name=repo, owner=u)
-    gh = github3.GitHub(token=u.access_token)
-    gh_repo = gh.repository(u.username, b.name)
-
+    commits_data = []
     for commit in commits:
-        data = {"hash": commit['id'], "title": commit['message'],
-                     "blog": repo, "username": username, "token": u.access_token}
-        c = Commit(hash=data['hash'], title=data['title'], blog=b)
-        c.save()
-        commit_additional_data = gh_repo.commit(data['hash'])
-        data.update({"files": commit_additional_data.as_dict()['files']})
-        sync_posts.delay(data)
+        commits_data.append({"hash": commit['id'], "title": commit['message'],
+                            "blog": repo, "username": username})
+    sync_commits.delay(commits_data)
