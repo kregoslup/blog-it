3eb76b008b1be89635a90a5434cdea325f1eb156
kregoslup <mateusz.olczynski@gmail.com>
2016-03-07 18:14:07 +0100

    Add routers, refactor urls

--- urls.py
+++ urls.py
@@ -1,7 +1,21 @@
 from django.conf.urls import url, include
-from apps.blog.views import profile_info
+from apps.blog.views import *
+from rest_framework_nested import routers
+from apps.blog.views import BlogsList, profile_info
+from apps.posts.views import PostViewSet
+
+
+router = routers.SimpleRouter()
+router.register(r'blogs', BlogsList)
+
+blogs_router = routers.NestedSimpleRouter(router, r'blogs', lookup='blog')
+blogs_router.register(r'posts', PostViewSet, base_name='blog-posts')
+
 
 urlpatterns = [
+    url(r'^github/login/$', oauth_login),
+    url(r'^github/login/callback/$', oauth_callback),
     url(r'^profile/$', profile_info),
-    url(r'^api/', include('apps.blog.urls')),
+    url(r'^', include(router.urls)),
+    url(r'^', include(blogs_router.urls)),
 ]
