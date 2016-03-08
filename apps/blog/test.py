eed6e8966a700d76ad7ceb6fd3589c31ae0470b4
kregoslup <mateusz.olczynski@gmail.com>
2016-03-08 14:39:03 +0100

    Blog & user tests

--- apps/blog/test.py
+++ apps/blog/test.py
@@ -15,47 +15,37 @@ SessionStore = import_module(settings.SESSION_ENGINE).SessionStore
 
 class BlogSerializerTest(APITestCase):
     def test_get_blogs(self):
-        response = self.client.get('/api/blogs/')
+        response = self.client.get('/blogs/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_create_test_blog(self):
-        u = User(username="testuser", access_token="accesstoken")
+    def test_create_blog_post(self):
+        u = User(username='test', access_token='testtoken')
         u.save()
-        data = {"title": "testitle",
-                "name": "testname",
-                "owner": u.pk}
-        serializer = BlogSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-        self.assertEqual(Blog.objects.count(), 1)
-
-    def test_api_create_blog(self):
-        u = User(username="testuser", access_token="accesstoken")
-        u.save()
-        data = {"title": "testitle",
-                "name": "testname",
-                "owner": u.pk}
-        response = self.client.post('/api/blogs/', data=data)
+        response = self.client.post('/blogs/', data={"title": "test", "owner": u.pk,
+                                                     "name": "test"})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_get_blog(self):
-        u = User(username="testuser", access_token="accesstoken")
+    def test_get_created_blog(self):
+        u = User(username='test', access_token='testtoken')
         u.save()
-        b = Blog(id=1, title="sampetitle", name="samplename", owner=u)
-        b.save()
-        response = self.client.get('/api/blogs/1/')
-        self.assertEqual(response.data, {"id": 1,
-                                         "title": "sampetitle",
-                                         "name": "samplename",
-                                         "owner": u.pk})
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response = self.client.post('/blogs/', data={"title": "test", "owner": u.pk,
+                                    "name": "test"})
+        b = Blog.objects.get(title='test')
+        response = self.client.get('/blogs/' + str(b.pk), follow=True)
+        self.assertEqual(response.data, {'name': 'test', 'id': 2, 'title': 'test',
+                                         'owner': 2})
 
 
-class UserSerializerTest(APITestCase):
-    def test_create_user(self):
-        data = {"username": "testusername",
-                "access_token": "testoken"}
-        serializer = UserSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-        self.assertEqual(User.objects.count(), 1)
+class UserCreateTest(APITestCase):
+    def testcase(self):
+        u = User(username="testowy", access_token="pleple")
+        u.save()
+        data = {"username": 'testowy', "access_token": 'pleple1'}
+        ser = UserSerializer(data=data)
+        if ser.is_valid():
+            obj, created = User.objects.update_or_create(username="testowy", defaults=ser.validated_data)
+            obj.save()
+            u = User.objects.get(access_token='pleple1')
+            response = Response(data=ser.data)
+            self.assertEqual(response.data, ser.data)
+            self.assertEqual(User.objects.count(), 1)
