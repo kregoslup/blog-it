531ffc2d90b4000a536ed7b66765809bd2e2bf87
kregoslup <mateusz.olczynski@gmail.com>
2016-03-10 12:48:13 +0100

    Add chaining

--- apps/posts/tasks.py
+++ apps/posts/tasks.py
@@ -3,51 +3,64 @@ from apps.posts.models import Commit, Post
 from celery.task import task
 import github3
 from github.repos import download_file
-from django.core.exceptions import ObjectDoesNotExist
-
+from celery import chain
 
 # TODO: Sanity checks
 
 
 @task
-def sync_posts(data):
-    u = User.objects.get(username=data['username'])
-    b = Blog.objects.get(name=data['repo'], owner=u)
-    posts_bulk_create_data = []
-    for file in data['files']:
-        post_body = download_file(file['raw_url'], u.access_token)
-        posts_bulk_create_data.append(Post(title=file['filename'].split('.')[-1],
-                                           body=post_body, blog=b, author=u))
-    Post.objects.bulk_create(posts_bulk_create_data,
-                             batch_size=len(posts_bulk_create_data))
+def sync_posts(username, repo_name, commits_data):
+    u = User.objects.get(username=username)
+    b = Blog.objects.get(name=repo_name, owner=u)
+    added = set()
+    removed = set()
+    for commit in commits_data:
+        for files in commit['files']:
+            if '.' in files['filename']:
+                filename = files['filename'].split('.')[-1],
+            else:
+                filename = files['filename']
+            if files['status'] is 'modified' and files['filename'] not in added:
+                added.add(files['filename'])
+                post_body = download_file(files['contents_url'], u.access_token)
+                Post.objects.filter(title=filename, blog=b).update(body=post_body)
+            elif files['status'] is 'added' and files['filename'] not in added:
+                added.add(files['filename'])
+                obj, created = User.objects.get_or_create(username=commit['commit_author'],
+                                                          defaults={"username": commit['commit_author']})
+                post_body = download_file(files['contents_url'], u.access_token)
+                Post.objects.create(title=filename, body=post_body,
+                                    blog=b, author=obj)
+            elif files['status'] is 'removed' and files['filename'] not in added:
+                removed.add(filename)
+                Post.objects.filter(title=filename, blog=b).delete()
 
 
 @task
-def sync_commits(data):
-    u = User.objects.get(username=data['username'])
-    b = Blog.objects.get(name=data['repo'], owner=u)
-    commit_bulk_create_data = []
-    for commit in data:
-        commit_bulk_create_data.append(Commit(hash=commit['hash'],
-                                              title=commit['title'], blog=b))
-    if commit_bulk_create_data:
-        Post.objects.bulk_create(commit_bulk_create_data,
-                                 batch_size=len(commit_bulk_create_data))
-
+def sync_commits(username, repo_name, commits_data):
+    u = User.objects.get(username=username)
+    b = Blog.objects.get(name=repo_name, owner=u)
     gh = github3.GitHub(token=u.access_token)
     gh_repo = gh.repository(u.username, b.name)
-    commit_additional_data = gh_repo.commit(data['hash'])
-    data.update({"files": commit_additional_data.as_dict()['files']})
-    sync_posts.delay(data)
+    commit_bulk_create_data = [
+        Commit(hash=commit['hash'], title=commit['title'],
+               blog=b) for commit in commits_data]
+    Commit.objects.bulk_create(commit_bulk_create_data,
+                               batch_size=len(commit_bulk_create_data))
+    for commit in commits_data:
+        commit.update(
+            {"files": gh_repo.commit(commit['hash']).as_dict()['files']})
+
+    return username, repo_name, commits_data.sort(key=lambda x: x['timestamp'])
 
 
 @task
-def parse_webhook(request_data):
-    commits = [commit for commit in request_data['commits']]
-    repo = request_data["repository"]['name']
-    username = request_data['owner']['name']
-    commits_data = []
-    for commit in commits:
-        commits_data.append({"hash": commit['id'], "title": commit['message'],
-                            "blog": repo, "username": username})
-    sync_commits.delay(commits_data)
+def parse_webhook(username, repo_name, commits):
+    commits_data = [{"hash": commit['id'], "title": commit['message'],
+                     "timestamp": commit['timestamp'],
+                     "commit_author": commit['committer']['username']}
+                    for commit in commits]
+    return username, repo_name, commits_data
+
+
+res = chain(parse_webhook(), sync_commits(), sync_posts())
