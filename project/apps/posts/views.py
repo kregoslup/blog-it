11a4af7fc397b2efdc0fd87e7d4042d87a3b2934
krego <mateusz.olczynski@gmail.com>
2017-02-06 17:52:25 +0100

    Move webhook api endpoint to class view

--- project/apps/posts/views.py
+++ project/apps/posts/views.py
@@ -1,3 +1,5 @@
+from rest_framework.generics import CreateAPIView
+
 from project.apps.posts.serializers import PostSerializer
 from project.apps.posts.tasks import res
 from rest_framework import status
@@ -13,14 +15,14 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
 
 
-@api_view(['POST'])
-def webhook(request):
-    request_data = request.body
-    if "zen" in request_data.keys() or not request_data['ref'].endswith('master'):
-        return Response(status=status.HTTP_200_OK)
-    else:
-        commits = [commit for commit in request_data['commits']]
-        repo_name = request_data["repository"]['name']
-        username = request_data['repository']['owner']['name']
-        res.apply_acync(username, repo_name, commits)
-        return Response(status=status.HTTP_200_OK)
+class WebhookCreateAPIView(CreateAPIView):
+    def post(self, request, *args, **kwargs):
+        request_data = request.body
+        if "zen" in request_data.keys() or not request_data['ref'].endswith('master'):
+            return Response(status=status.HTTP_200_OK)
+        else:
+            commits = [commit for commit in request_data['commits']]
+            repo_name = request_data["repository"]['name']
+            username = request_data['repository']['owner']['name']
+            res.apply_acync(username, repo_name, commits)
+            return Response(status=status.HTTP_200_OK)
